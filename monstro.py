494d16d66e94b208bbbbbfda5bacdf5fb3d20325
Jonathan Thai <36181811+jonathanht@users.noreply.github.com>
2020-01-08 16:36:55 -0800

    Update monstro.py

--- monstro.py
+++ monstro.py
@@ -1,6 +1,6 @@
-#bot.py #replace input() with message
-import os, discord, bs4, requests
-
+#bot.py 
+import os, discord, bs4
+import requests
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -12,17 +12,21 @@ client = commands.Bot(command_prefix = '!')
 
 @client.command(pass_context = True)
 async def bday(ctx, char):
-    res = requests.get('https://stardewvalley.wiki.com/' + char.lower())
+    entryName = char
+
+
+    res = requests.get('https://stardewvalleywiki.com/' +  entryName.lower())
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
     elems = soup.select('#infoboxdetail')
-    birthday = elems[0].text.strip()
-    
 
-    await ctx.send(char.lower() + " 's birthday is on: " + birthday)
+    await ctx.send(entryName.lower() + "'s birthday is on: " + elems[0].text.strip())
 
 
-
+@client.command(pass_context = True)
+async def ping(ctx, message):
+    await ctx.send(message)
 
 
 @client.command(pass_context = True)
@@ -30,13 +34,12 @@ async def slice(ctx, person):
     await ctx.send("https://tenor.com/view/kimetsu-no-yaiba-zenitsu-demon-slayer-lightning-breathing-iai-gif-14394969 \nthis is totally me when i'm slicing " + person + "'s head off")
 
 
+
+
 @client.event
 async def on_ready():
 	print(f'{client.user} has connected to Discord!')
 
-
-
-
-
-
-client.run("INSERT BOT TOKEN HERE")
+	
+	
+client.run(BOT TOKEN HERE) 
